d78ecbd351ec64c05ff0d13a61f89457b8d3588b
hirnic <128711578+hirnic@users.noreply.github.com>
2024-12-28 10:28:49 -0600

    Add files via upload

--- Quaternion Kinematics/PoseFinder.py
+++ Quaternion Kinematics/PoseFinder.py
@@ -13,17 +13,19 @@ TableID = [DQClass.ToVectorQuaternion(x) for x in TableID]
 # init is a dual quaternion pose, and lengths is an array of 6 floats. Output is a dual quaternion pose.
 def PoseFinder(init, lengths):
     X, Y = init, DQClass.ZeroDQ()
-    while (X-Y).size() > 10**(-12):
+    j = 0
+    while (X-Y).size() > 10**(-4):
         Y = X
         f, Lf = [], []
-        for n in range(6):                                              # This loop finds the function and derivative
-            s = (X.A * TableID[n] + X.B * 2) * X.A.conjugate()          # Formula (8), page 4
-            f.append((s - Base[n]).norm() - lengths[n])                 # Formula (80), page 16
-            u = np.array((s - Base[n]).normalization().ToPureVec())     # Formula (66), page 13, measured in fixed frame
-            Cross = np.cross(np.array(s.ToPureVec()), u)                # Formula (67), page 13
-            Lf.append(np.concatenate([Cross, u]) * 2)                   # Formula (68), page 14
+        for n in range(6):  # This loop finds the function and derivative
+            s = (X.A.conjugate() * Base[n] + X.B.conjugate() * 2) * X.A           # Formula (8), page 4
+            f.append((TableID[n] - s).norm() - lengths[n])                        # Formula (80), page 16
+            u = np.array((TableID[n] - s).normalization().ToPureVec())      # Formula (66), page 13,
+            Cross = np.cross(np.array(TableID[n].ToPureVec()), u)                 # Formula (67), page 13
+            Lf.append(np.concatenate([Cross, u]) * 2)                             # Formula (68), page 14
         LInv = np.linalg.inv(np.array(Lf)) * (-1)
-        Theta = DQClass.ToVectorDualQuaternion(LInv.dot(np.array(f)))   # Equation (80), page 16
-        Hat = (DQClass.IdentityDQ() + Theta).normalization()            # Formula (79), page 16
-        X = Hat * X                                                     # Formula (79), page 16 adapted to F.o.R.
-    return X
+        Theta = DQClass.ToVectorDualQuaternion(LInv.dot(np.array(f)))             # Equation (80), page 16
+        Hat = (DQClass.IdentityDQ() + Theta).normalization()                      # Formula (79), page 16
+        X = X * Hat                                                               # Formula (79), page 16
+        j += 1
+    return [j,X]
